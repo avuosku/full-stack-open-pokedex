8d3b3b0c32a31ac95833f2bf3d360772d52b4ce7
avuosku <arttu.vuosku@gmail.com>
2025-06-13 13:09:12 +0300

    Fix ESLint errors

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require('cypress')
+/* eslint-env node */
+
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000',
-    supportFile: false,
-    specPattern: 'cypress/e2e/**/*.cy.js'
-  }
-})
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
--- cypress/e2e/frontpage.cy.js
+++ cypress/e2e/frontpage.cy.js
@@ -1,7 +1,8 @@
-describe('Pokedex', function () {
-  it('front page can be opened', function () {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
-})
+/* eslint-env cypress */
+
+describe('Front page', () => {
+  it('shows correct title', () => {
+    cy.visit('http://localhost:5000');
+    cy.contains('Pokédex');
+  });
+});
